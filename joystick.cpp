9a8ffbf639675d156b37dd9578284881ed8e0346
mengbishu <841616394@qq.com>
2018-03-15 14:52:04 +0800

    1

---
+++ joystick.cpp
@@ -0,0 +1,24 @@
+#include "pxt.h"
+
+using namespace pxt;
+namespace gamerpad {
+    bool initialized = false;
+
+    //%
+    void init() {
+        if (initialized) return;
+
+    // mount buttons on the pins with a pullup mode
+    // TODO: fix this issue in the DAL itself
+#define ALLOC_PIN_BUTTON(id) new MicroBitButton(getPin(id)->name, id, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
+//    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P5)
+//    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P16)
+    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P12)
+    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P8)
+    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P1)
+    ALLOC_PIN_BUTTON(MICROBIT_ID_IO_P2)
+#undef ALLOC_PIN_BUTTON
+
+        initialized = true;
+    }
+}
--- main.ts
+++ main.ts
@@ -1,7 +1,123 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * User Buttons for DFRobot gamer:bit Players.
+ */
+//%
+enum GamerBitPin {
+    //% block="Z button"
+    P8 = <number>DAL.MICROBIT_ID_IO_P8,
+}
+
+namespace joystick { 
+
+    let PIN_INIT = 0;
+
+    export enum Vibrator { 
+        //% blockId="V0" block="stop"
+        V0 = 0,
+        //% blockId="V1" block="Vibration"
+        V1 = 255,     
+    }
+
+    export enum Led {
+        //% blockId="OFF" block="off"
+        OFF = 0,
+        //% blockId="ON" block="on"
+        ON = 1
+    }
+
+    enum GamerBitEvent {
+        //% block="pressed"
+        Down = DAL.MICROBIT_BUTTON_EVT_DOWN,
+        //% block="released"
+        Up = DAL.MICROBIT_BUTTON_EVT_UP,
+        //% block="click"
+        Click = DAL.MICROBIT_BUTTON_EVT_CLICK,
+    }
+
+    //% shim=joystick::init
+    function init(): void { 
+        return;
+    }
+
+    function PinInit(): void { 
+        pins.setPull(DigitalPin.P1, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P2, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P8, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P5, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P11, PinPullMode.PullNone);
+        pins.setPull(DigitalPin.P16,  PinPullMode.PullUp);
+        PIN_INIT = 1;
+        return;
+    }
+
+    //% weight=70
+    //% blockId=joystick_keyState block="button|%button|is pressed"
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
+    function pressed(button: GamerBitPin): boolean { 
+        if (!PIN_INIT) { 
+            PinInit();
+        }
+        let num = false;
+        if (0 == pins.digitalReadPin(<number>button)) {
+            num = true;
+        }
+        return num;
+    }
+
+    /**
+     * Registers code to run when a DFRobot gamer:bit event is detected.
+     */
+    //% weight=60
+    //% blockGap=50
+    //% blockId=joystick_onEvent block="on button|%button|is %event"
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
+    //% event.fieldEditor="gridpicker" event.fieldOptions.columns=3
+    export function onEvent(button: GamerBitPin, event: GamerBitEvent, handler: Action) {
+        init();
+        if (!PIN_INIT) { 
+            PinInit();
+        }
+        control.onEvent(<number>button, <number>event, handler); // register handler
+    }
+
+
+    /**
+     * Vibrating motor switch.
+     */
+    //% weight=50
+    //% blockId=joystick_vibratorMotor block="Vibrator motor switch|%index|"
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    export function vibratorMotor(index: Vibrator): void {
+        vibratorMotorSpeed(<number>index);
+        return;
+    }
+
+    /**
+     * Vibration motor speed setting, adjustable range 0~255.
+     */
+    //% weight=30
+    //% blockGap=50
+    //% blockId=joystick_vibratorMotorSpeed block="Vibrator motor intensity|%degree"
+    //% degree.min=0 degree.max=255
+    export function vibratorMotorSpeed(degree: number): void {
+        if (!PIN_INIT) { 
+            PinInit();
+        }
+        let num = degree * 4;
+        pins.analogWritePin(AnalogPin.P12, <number>num);
+        return;
+    }
+
+    /**
+     * LED indicator light switch.
+     */
+    //% weight=20
+    //% blockId=joystick_led block="LED|%index|"
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    export function led(index: Led): void {
+        if (!PIN_INIT) { 
+            PinInit();
+        }
+        pins.digitalWritePin(DigitalPin.P16, <number>index);
+    }
+}
