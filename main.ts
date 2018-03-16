578e808d4c32628bf2454dd12db6ecabc73b726f
mengbishu <841616394@qq.com>
2018-03-16 14:25:31 +0800

    1

--- main.ts
+++ main.ts
@@ -137,8 +137,8 @@ namespace joystick {
     //% blockGap=40
     //% blockId=detect block="joystick|%read_|%compare_|%value_"
     //% value.min=-10 value.max=10
-    export function detect(read_: number, compare_: number, value_: number): boolean { 
- 
+    export function detect(read_: read, compare_: compare, value_: number): boolean { 
+
         return false;
     }
     
