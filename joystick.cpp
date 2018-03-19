1d3d8e453b0f834225bbe18591dc97bd951b995c
mengbishu <841616394@qq.com>
2018-03-19 16:30:53 +0800

    1

--- joystick.cpp
+++ joystick.cpp
@@ -32,7 +32,7 @@ namespace joystick {
         }
     }
 
-    void Shake(int pin,int value,Action a) {
+    void Shake(Action a) {
         if (a != 0) { 
             incr(a);
             create_fiber(forever_stubs, (void*)a);
