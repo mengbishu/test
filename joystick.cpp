6594f882592cc2a3ab3861a5117f8e5adc87eea9
mengbishu <841616394@qq.com>
2018-03-19 17:42:05 +0800

    1

--- joystick.cpp
+++ joystick.cpp
@@ -28,11 +28,11 @@ namespace joystick {
     void Shake(int pin,int value,Action a) {
         if (a != 0) { 
             incr(a);
-//            while (true) {
+            while (true) {
 //                value = pins.analogReadPin(pin);
                 runAction0((Action)a);
                 fiber_sleep(50);
-//            }
+            }
         }
     }
 }
